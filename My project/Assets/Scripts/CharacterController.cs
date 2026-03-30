9df28af100ab51320a08cec769b0bfaa2448a12b
Camron Gonzalez <camrong520@gmail.com>
2026-03-30 10:15:32 -0400

    set up input action.

--- My project/Assets/Scripts/CharacterController.cs
+++ My project/Assets/Scripts/CharacterController.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem; 
 
 public class CharacterController : MonoBehaviour
 {
+    private InputAction _moveAction;
+
+    public float moveSpeed = 5f; 
+    
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Starting Play");
+        _moveAction = InputSystem.actions.FindAction("Move"); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 input = _moveAction.ReadValue<Vector2>();
         
+        Debug.Log(input.ToString());
     }
 }
